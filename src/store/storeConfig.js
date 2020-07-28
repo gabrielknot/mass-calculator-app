dd88b90d42e718346eda5842e5cb8ec20a3161c3
gabrielknot <gcftutos@gmail.com>
2020-07-28 00:02:38 -0300

    Redux configuration

---
+++ src/store/storeConfig.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers} from "redux"
+
+const reducers = combineReducers({
+    numbers: function (state, action){
+        return{
+            Height: 0,
+            Waist: 0,
+            hip: 0,
+            Neck: 0
+        },
+    isMale: function(state,action){
+        return true
+    }
+    }
+})
+
+export default function storeConfig(){
+    return createStore(reducers)
+}
