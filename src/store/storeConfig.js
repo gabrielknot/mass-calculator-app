f01876b393360b6714c4b1e4a15043da2f904c98
gabrielknot <gcftutos@gmail.com>
2020-07-28 01:08:21 -0300

    Remooving debug elements

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,8 +2,6 @@ import { createStore, combineReducers} from "redux"
 
 const reducers = combineReducers({
     numbers: function (state, action){
-<<<<<<< HEAD
-        console.log("aaaaaaaaaaa")
         return{
             Height: 0,
             Waist: 0,
@@ -11,22 +9,11 @@ const reducers = combineReducers({
             Neck: 0
         }
     },
+    
     isMale: function(state,action){
         return true
     }
-=======
-        return{
-            Height: 0,
-            Waist: 0,
-            hip: 0,
-            Neck: 0
-        },
-    isMale: function(state,action){
-        return true
-    }
-    }
->>>>>>> dd88b90d42e718346eda5842e5cb8ec20a3161c3
-})
+    })
 
 export default function storeConfig(){
     return createStore(reducers)
