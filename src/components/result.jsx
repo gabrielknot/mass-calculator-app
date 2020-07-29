a645e79741db002066e78b7f1d0aea47407f6bf4
gabrielknot <gcftutos@gmail.com>
2020-07-29 01:28:33 -0300

    Redux dispatch config. Add result component

---
+++ src/components/result.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from "react-redux"
+
+function Result(props){
+    var result = 495/(1.03224-0.19077*(Math.log10(props.waist+props.hip-props.neck)) + 0.15566*(Math.log10(
+        props.height
+    ))) - 450 
+    if (isNaN(result)) result = 0;
+    return(
+        <div className="result">
+            <h1>{result}</h1>
+        </div>
+    )
+}
+
+function mapStateToProps(state){
+    return{
+        height: state.numbers.Height,
+        waist: state.numbers.Waist,
+        neck: state.numbers.Neck,
+        hip: state.numbers.Hip
+    }
+}
+
+export default connect(mapStateToProps)(Result)
