5c353a44e87b2aa319ba165e66047e59164387bc
gabrielknot <gcftutos@gmail.com>
2020-07-30 18:47:58 -0300

    Add toFixed(2) to the float number from the result

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -5,10 +5,11 @@ function Result(props){
     var result = (495/(1.03224-0.19077*(Math.log10(props.waist+props.hip-props.neck)) + 0.15566*(Math.log10(
         props.height
     ))) - 450 )+3
+
     if (isNaN(result)) result = 0;
     return(
         <div className="result">
-            <h1>{result}</h1>
+            <h1>{result.toFixed(2)}</h1>
         </div>
     )
 }
