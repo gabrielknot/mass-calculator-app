9d383eaded9778913554d05ec674e701080eb612
gabrielknot <gcftutos@gmail.com>
2020-07-31 20:51:00 -0300

    Adding the iconButton component

---
+++ src/template/iconButton.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+
+export default class IconButton extends Component{
+    constructor(props) {
+        super(props);
+        
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+    handleInputChange(event) {
+        
+    }
+
+    render(){
+        if(this.props.checkbox){
+            return <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.props.checked}
+                        onChange={this.props.onClick} 
+                    />
+        }
+        if(this.props.hide){
+            return null
+        }else{
+        return(
+            <button className={'btn btn-'+this.props.style} onClick={this.props.onClick}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+}
